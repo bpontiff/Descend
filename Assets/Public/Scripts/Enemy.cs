cb6fd7260c2920ca553858b67641e4ab91b98514
Becky Pontiff <bpontiff@gmail.com>
2019-08-17 21:16:05 -0500

    working on new health system

--- Assets/Public/Scripts/Enemy.cs
+++ Assets/Public/Scripts/Enemy.cs
@@ -5,15 +5,6 @@ namespace Assets.Public.Scripts
 {
     class Enemy : Actor
     {
-        void OnCollisionEnter2D(Collision2D col)
-        {
-            if ("Player".Equals(col.gameObject.tag))
-            {
-                    Player player = col.gameObject.GetComponent<Player>();
-                    player.health -= 1; // TODO: enemies need weapons to attack with so this is temporary. 
-            }
-
-        }
 
     }
 }
--- Assets/Public/Scripts/Player.cs
+++ Assets/Public/Scripts/Player.cs
@@ -6,34 +6,9 @@ namespace Assets.Public.Scripts
 {
     class Player : Actor
     {
-        public Image[] hearts;
-        public Sprite fullHeart;
-        public Sprite emptyHeart;
 
         void Update ()
         {
-            if (health > maxHealth)
-            {
-                health = maxHealth;
-            }
-
-            for (int i = 0; i < hearts.Length; i++) {
-                if (i < health)
-                {
-                    hearts[i].sprite = fullHeart;
-                } else
-                {
-                    hearts[i].sprite = emptyHeart;
-                }
-
-                if (i < maxHealth)
-                {
-                    hearts[i].enabled = true;
-                } else
-                {
-                    hearts[i].enabled = false;
-                }
-            }
         }
     }
 }
