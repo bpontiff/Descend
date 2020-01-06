3813da4da40b8b78355cab46ede7f9ae6a80b0be
conansgold@hotmail.com <conandoesgaming@gmail.com>
2020-01-06 08:20:15 -0600

    Pushing work over December

---
+++ Assets/PopupText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupText : MonoBehaviour
+{
+    public Color32 color = new Color(0.8f, 0.8f, 0, 1.0f);
+    public float scroll = 0.05f;  // scrolling velocity
+     public float duration = 1.5f; // time to die
+     public float alpha;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<UnityEngine.UI.Text>().material.color = color; // set text color
+        alpha = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (alpha > 0)
+        {
+            transform.position.Set(transform.position.x, scroll * Time.deltaTime, transform.position.z);
+            //alpha -= Time.deltaTime / duration;
+            //color.a  = alpha;
+        }
+        else
+        {
+            //Destroy(gameObject); // text vanished - destroy itself
+        }
+    }
+}
---
+++ Assets/Public/Scripts/Menus and UI/Camera & Sorting/OverlayObjMovingSortOrdering.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class OverlayMovingSortOrdering : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        //Sort the sprite based on the location it is in the Y axis
+        GetComponentInChildren<SpriteRenderer>().sortingOrder = (Mathf.RoundToInt(transform.position.y * 100f) * -1) + 5;
+    }
+}
